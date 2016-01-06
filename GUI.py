2a2c1b7278f2ea39effd6f62395f0eedcda2f513
Andreas Käll <andreas.kall@live.com>
2016-01-07 00:53:32 +0100

    using "gridSpec" instead of "subplot2grid" and changed map image

--- GUI.py
+++ GUI.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python
 import sys
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 import matplotlib.animation as animation
 from matplotlib import style
 
 
+####### HTML snippet to retrieve image of queen elizabeth olympic park
+####### https://maps.googleapis.com/maps/api/staticmap?center=51.5399,-0.012778&zoom=16&size=600x550&scale=2&maptype=satellite
+#######
 
 
 #adjust these values based on your location and map, lat and long are in decimal degrees
-TRX = 11.979496          #top right longitude
-TRY = 57.695401          #top right latitude
-BLX = 11.967124          #bottom left longitude
-BLY = 57.691521          #bottom left latitude
-lat_input = 0            #latitude of home marker
-long_input = 0           #longitude of home marker
+TRX = 11.978420         #top right longitude
+TRY = 57.696324         #top right latitude
+BLX = 11.965645         #bottom left longitude
+BLY = 57.690052         #bottom left latitude
+lat_input = 0           #latitude of home marker
+long_input = 0          #longitude of home marker
 
 mapImage = plt.imread('map.png')
 
@@ -22,9 +26,14 @@ plt.rcParams['toolbar'] = 'None'
 style.use('fivethirtyeight')
 
 fig = plt.figure()
-temperature = plt.subplot2grid((3, 4), (1, 0), colspan=2)
-speedFig = plt.subplot2grid((3, 4), (0, 0), colspan=2)
-positionFig = plt.subplot2grid((3,4),(0,2), colspan=2, rowspan=2)
+gs1 = gridspec.GridSpec(3, 3)
+gs1.update(left=0.05, right=0.49, wspace=0.05)
+temperature = plt.subplot(gs1[1, :])
+speedFig    = plt.subplot(gs1[0, :])
+
+gs2 = gridspec.GridSpec(4, 1)
+gs2.update(left=0.5, right=0.99, wspace=0)
+positionFig = plt.subplot(gs2[:,:1])
 
 def animate(i):
 	graph_data = open('data.txt','r').read()
@@ -67,19 +76,18 @@ def animate(i):
 
 	position = (lines[-2].split(',')[11], lines[-2].split(',')[12])
 	positionFig.clear()
-	positionFig.set_title('Position')
 	plt.imshow(mapImage,extent=[BLX, TRX, BLY, TRY])
 	positionFig.grid(False)
 	positionFig.set_xticks([])
 	positionFig.set_yticks([])
-	w,h = len(mapImage[0]), len(mapImage) 
-	aspect = float(w)/h
-	positionFig.set_aspect(aspect)
+	#w,h = len(mapImage[0]), len(mapImage) 
+	#aspect = float(w)/h
+	positionFig.set_aspect(2)
 
 	if position[0] != 'None':
 		positionFig.set_xlabel('Connected')
 		position = (float(position[0]), float(position[1]))
-		positionFig.scatter(x=[position[1]], y=[position[0]], s = 20, c='r')
+		positionFig.scatter(x=position[1], y=position[0], s = 50, c='r')
 	else:
 		positionFig.set_xlabel('Not connected')
 		
